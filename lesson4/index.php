2e0c0dd86f1cb1174fcf033ea088bc4fbaaa5e61
skrasavin <s-krasssavin@mail.ru>
2018-12-08 16:55:09 +0300

    add lesson4

--- lesson4/index.php
+++ lesson4/index.php
@@ -7,18 +7,28 @@
 </head>
 <body>
 
-<a href="photos/1.jpg" target="_blank"><img class="one" src="photos/1.jpg" width="500" height="300"></a>
-<a href="photos/2.jpg" target="_blank"><img class="one" src="photos/2.jpg" width="500" height="300"></a>
-<a href="photos/3.jpg" target="_blank"><img class="one" src="photos/3.jpg" width="500" height="300"></a>
-<a href="photos/4.jpg" target="_blank"><img class="one" src="photos/4.jpg" width="500" height="300"></a>
-<a href="photos/.$_FILES[photo][name].jpg" target="_blank"><img class="one" src="photos/.$_FILES[photo][name].jpg" +
-                                                                width="500" height="300"></a>:
-
 <?
 
-include "server.php";
+$photos = scandir("photos/");
+
+$count = 2;
+$eoa = count($photos);
+
+echo $eoa;
+
+
+while($count < $eoa){
+    getTag($photos[$count]);
+    $count++;
+}
+
+function getTag($position) {
+    echo $tagA = "<a href=\"photos/$position\" target=\"_blank\"><img class=\"one\" src=\"photos/$position\" width=\"500\" height=\"300\"></a>";
+}
 
 ?>
+
+
 
 <form method="POST" action="server.php" enctype="multipart/form-data">
     <p>Загрузите файл</p>
--- lesson4/server.php
+++ lesson4/server.php
@@ -6,6 +6,7 @@ echo $path;
 
 
 if(move_uploaded_file($_FILES["photo"]["tmp_name"], $path)) {
+
     echo "Файл успешно загружен";
 }
 //else {
