a49999d28b0a3578d05f6c7b20c4888309d65445
skrasavin <s-krasssavin@mail.ru>
2019-01-19 16:33:56 +0300

    'addlesson4'

--- php2/lesson4/connect.php
+++ php2/lesson4/connect.php
@@ -1,12 +1,53 @@
-<?
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'php2lesson4');
+define('PORT', '3305');
+define('USER_NAME', 'root');
+define('USER_PASS', '');
 
-$link = mysqli_connect("127.0.0.1", "root", "", "php2lesson4");
-$select = "SELECT * FROM `photos` LIMIT 3";
-$res = mysqli_query($link, $select);
-$stack[] = Null;
-$s = 1;
+function showTwig($stack) {
+    require_once 'Twig/Autoloader.php';
+    Twig_Autoloader::register();
 
-while ($data = mysqli_fetch_assoc($res)) {
-    array_push($stack, 'photos/' . $data['addr']);
+    try {
+        $loader = new Twig_Loader_Filesystem('templates');
+
+        $twig = new Twig_Environment($loader);
+
+        $template = $twig->loadTemplate('list.tmpl');
+
+        echo $template->render(array (
+            'items' => $stack
+        ));
+    } catch (Exception $e) {
+        die ('ERROR: ' . $e->getMessage());
+    }
 }
-$stack = array_slice($stack, '1');
+function render($c) {
+    $stack = [];
+
+    try {
+        $db = new PDO('mysql:host=' . DB_HOST . ';port='. PORT . ';dbname=' . DB_NAME .';', USER_NAME, USER_PASS );
+        $sth = $db->query('SELECT * FROM `photos` LIMIT ' . $c);
+
+        while ($data = $sth -> fetch()) {
+            echo $data['id'].'/';
+            if($data['id'] == 50 || $data['id'] == 100 ) {
+                echo '<br>';
+            }
+            array_push($stack, 'photos/'.$data['addr']);
+        };
+
+    }catch (PDOException $e) {
+        echo $e;
+    }
+    showTwig($stack);
+}
+
+if(isset($_POST['postname'])) {
+    $c = $_POST['postname'];
+    render($c);
+}
+
+
+
--- php2/lesson4/index.php
+++ php2/lesson4/index.php
@@ -1,28 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<button id="searchip">Ещё фото</button>
+<div id="resultip"></div>
 
-<form method="post" name="form">
-    <input name="submit" type="submit" value="Добавить">
-</form>
+<script>
+    var count = 25;
+    $(document).ready(function () {
+        $.post('connect.php',{postname:count},
+            function (data){
+                $("#resultip").html(data);
+                count = 50;
+            }
+        );
+        $("#searchip").click(function () {
 
-<?php
-include_once 'connect.php';
-require_once 'Twig/Autoloader.php';
-Twig_Autoloader::register();
-
-try {
-    $loader = new Twig_Loader_Filesystem('templates');
-
-    $twig = new Twig_Environment($loader);
-
-    $template = $twig->loadTemplate('list.tmpl');
-
-    echo $template->render(array (
-        'items' => $stack
-    ));
-
-} catch (Exception $e) {
-    die ('ERROR: ' . $e->getMessage());
-}
+            $.post('connect.php',{postname:count},
+                function (data){
+                    $("#resultip").html(data);
+                    count += 25;
+                }
+            )
+        })
+    });
+</script>
 
 
-?>
-
+</body>
+</html>
